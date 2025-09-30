92b5edc5dce323d5b8bd07066de78c08bf497e2d
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-09-30 02:11:30 +0000

    refactor: mejorar ComboDirector con métodos predefinidos y builder inmutable

--- src/Model/ComboDirector.java
+++ src/Model/ComboDirector.java
@@ -3,17 +3,49 @@ package Model;
 import Builder.ComboBuilder;
 
 public class ComboDirector {
-    private ComboBuilder builder;
+    private final ComboBuilder builder;
 
     public ComboDirector(ComboBuilder builder) {
         this.builder = builder;
     }
 
+    /**
+     * Construcción genérica de un combo personalizado.
+     */
     public Combo construirCombo(String bebida, String acompanamiento, String extra) {
         return builder.withBebida(bebida)
                       .withAcompanamiento(acompanamiento)
                       .withExtra(extra)
                       .build();
     }
-}
 
+    /**
+     * Construye un combo clásico predefinido.
+     */
+    public Combo construirComboClasico() {
+        return builder.withBebida("Coca-Cola")
+                      .withAcompanamiento("Papas fritas")
+                      .withExtra("Helado")
+                      .build();
+    }
+
+    /**
+     * Construye un combo vegetariano predefinido.
+     */
+    public Combo construirComboVegetariano() {
+        return builder.withBebida("Agua mineral")
+                      .withAcompanamiento("Ensalada fresca")
+                      .withExtra("Fruta")
+                      .build();
+    }
+
+    /**
+     * Construye un combo de pollo predefinido.
+     */
+    public Combo construirComboPollo() {
+        return builder.withBebida("Inca Kola")
+                      .withAcompanamiento("Arroz chaufa")
+                      .withExtra("Salsa especial")
+                      .build();
+    }
+}
